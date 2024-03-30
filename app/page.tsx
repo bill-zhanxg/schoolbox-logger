96ed20ed1e645651e307c694a5d343a668d9d001
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2024-03-30 19:02:38 +1100

    replace default page

--- app/page.tsx
+++ app/page.tsx
@@ -3,69 +3,57 @@ export default function Home() {
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
 				<a
-					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					href="/azure-users"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
 				>
 					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Docs{' '}
+						Azure Users{' '}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
 					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Find in-depth information about Next.js features and API.
-					</p>
+					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Search all Azure users</p>
 				</a>
 
 				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+					href="/portraits"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
 				>
 					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Learn{' '}
+						Portraits{' '}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
 					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Learn about Next.js in an interactive course with&nbsp;quizzes!
-					</p>
+					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Search all portraits</p>
 				</a>
 
 				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					href="/combined"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
 				>
 					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Templates{' '}
+						Combined Search{' '}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
 					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Explore starter templates for Next.js.</p>
+					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Search all users and portraits</p>
 				</a>
 
 				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					href="/manage-data"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
 					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Deploy{' '}
+						Manage Data{' '}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
 					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-						Instantly deploy your Next.js site to a shareable URL with Vercel.
-					</p>
+					<p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>Mange all data</p>
 				</a>
 			</div>
 		</main>
