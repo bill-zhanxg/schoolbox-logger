7de83150e6afe1ae44323975f38b93bb44d504d1
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2024-12-25 20:02:30 +1100

    fix: now using neon db for development as well thanks to the stupid nextjs only allow edge for middleware

--- apps/web/libs/authConfig.ts
+++ apps/web/libs/authConfig.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@repo/database';
 import NextAuth from 'next-auth';
 import MicrosoftEntraID from 'next-auth/providers/microsoft-entra-id';
-import { prisma } from '@repo/database';
 
 export const authConfig = NextAuth({
 	adapter: PrismaAdapter(prisma),
@@ -13,7 +13,6 @@ export const authConfig = NextAuth({
 			clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
 			clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
 			profilePhotoSize: 648,
-			allowDangerousEmailAccountLinking: true,
 		}),
 	],
 	callbacks: {
@@ -24,36 +23,6 @@ export const authConfig = NextAuth({
 		async signIn({ user }) {
 			if (!user.email) return '/login?error=EmailRequired';
 			return true;
-		},
-	},
-	events: {
-		createUser: async (message) => {
-			// TODO: test
-			const { user } = message;
-			if (!user.id || !user.email) return;
-
-			const pendingUser = await prisma.pendingUsers.findUnique({
-				where: {
-					email: user.email,
-				},
-			});
-			if (!pendingUser) return;
-
-			await prisma.user.update({
-				where: {
-					id: user.id,
-				},
-				data: {
-					name: pendingUser.name,
-					role: pendingUser.role,
-				},
-			});
-
-			await prisma.pendingUsers.delete({
-				where: {
-					id: pendingUser.id,
-				},
-			});
 		},
 	},
 	pages: {
--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,3 +1,8 @@
+import { Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaNeon(neon);
+
+export const prisma = new PrismaClient({ adapter });
