b720e83a8d51344072f9431c916761f9267b6518
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2024-12-26 18:11:01 +1100

    feat: convert user page to prisma

--- apps/web/app/users/page.tsx
+++ apps/web/app/users/page.tsx
@@ -1,6 +1,6 @@
 import { auth } from '@/libs/auth';
 import { isAdmin } from '@/libs/checkPermission';
-import { getXataClient } from '@/libs/xata';
+import { prisma } from '@repo/database';
 import { Unauthorized } from '../globalComponents/Unauthorized';
 import { UserTable } from './components/UserTable';
 
@@ -8,12 +8,12 @@ export default async function Users() {
 	const session = await auth();
 	if (!session || !isAdmin(session)) return Unauthorized();
 
-	const users = await getXataClient().db.nextauth_users.select(['email', 'name', 'image', 'role']).getMany();
+	const users = await prisma.user.findMany();
 
 	return (
 		<UserTable
 			myId={session.user.id}
-			users={users.toSerializable().map((user) => ({
+			users={users.map((user) => ({
 				...user,
 				checked: false,
 			}))}
