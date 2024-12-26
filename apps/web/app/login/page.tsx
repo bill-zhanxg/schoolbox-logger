a898426811a0c011a8ee1736dc4b1582fc6e8a7c
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2024-12-26 16:30:07 +1100

    feat: improve login page

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,5 +1,11 @@
 import { auth, signIn } from '@/libs/auth';
+import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
+import { LoginBtn } from './components/LoginBtn';
+
+export const metadata: Metadata = {
+	title: 'Login',
+};
 
 type ErrorCodes =
 	| 'OAuthSignin'
@@ -11,6 +17,7 @@ type ErrorCodes =
 	| 'EmailSignin'
 	| 'CredentialsSignin'
 	| 'SessionRequired'
+	| 'EmailRequired'
 	| 'Default';
 
 export default async function Login(props: {
@@ -26,39 +33,39 @@ export default async function Login(props: {
 	const session = await auth();
 	if (session) return redirect(decodeURIComponent(callbackURL ?? '/'));
 
+	async function login() {
+		'use server';
+		await signIn('microsoft-entra-id');
+	}
+
 	return (
-		<div className="flex h-full flex-col items-center justify-center gap-3">
-			<h1 className="p-5 text-center text-4xl font-bold">Schoolbox Logger Login (Authorized access only)</h1>
-			<form
-				className="w-4/5 max-w-[20rem]"
-				action={async () => {
-					'use server';
-					await signIn('microsoft-entra-id');
-				}}
-			>
-				<button type="submit" className="btn btn-primary w-full">
-					Login with Microsoft
-				</button>
-			</form>
-			{/* Error */}
-			{(searchParams.message || searchParams.error) && (
-				<div className="alert alert-error w-4/5 max-w-[20rem]">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="h-6 w-6 shrink-0 stroke-current"
-						fill="none"
-						viewBox="0 0 24 24"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-						/>
-					</svg>
-					<span>Error: {searchParams.message || getMessages(searchParams.error)}</span>
+		<div className="flex h-full flex-col items-center justify-center">
+			<div className="flex flex-col items-center justify-center gap-4 rounded-lg p-10 shadow-xl">
+				<div>
+					<h1 className="text-center text-4xl font-bold">Schoolbox Logger Login (Authorized access only)</h1>
+					<p className="p-2 text-center text-gray-500">Make sure you use your school email</p>
 				</div>
-			)}
+				<LoginBtn login={login} />
+				{/* Error */}
+				{(searchParams.message || searchParams.error) && (
+					<div className="alert alert-error w-4/5 max-w-lg">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6 shrink-0 stroke-current"
+							fill="none"
+							viewBox="0 0 24 24"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+						<span>Error: {searchParams.message || getMessages(searchParams.error)}</span>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
@@ -83,6 +90,8 @@ function getMessages(error: ErrorCodes | string | string[] | undefined) {
 			return 'The authorize callback returned null in the Credentials provider.';
 		case 'SessionRequired':
 			return 'The content of this page requires you to be signed in at all times.';
+		case 'EmailRequired':
+			return "Your account does not have an e-mail address. Please make sure you're signing to your school account or contact the administrator.";
 		default:
 			return 'Unexpected error.';
 	}
