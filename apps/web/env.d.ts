fcb7e295b8478c17c1c5ae24f5c252d36ea6a15c
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2024-12-27 11:08:29 +1100

    feat: fully removed xata from frontend

--- apps/web/env.d.ts
+++ apps/web/env.d.ts
@@ -3,9 +3,6 @@ namespace NodeJS {
 		BASE_URL: string;
 		BACKEND_URL: string;
 
-		XATA_BRANCH: string;
-		XATA_API_KEY: string;
-
 		AUTH_SECRET: string;
 		AUTH_MICROSOFT_ENTRA_ID_ID: string;
 		AUTH_MICROSOFT_ENTRA_ID_SECRET: string;
